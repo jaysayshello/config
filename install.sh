306e3eaba8e2d2e6f4c2a70748e8f0574accb809
jay <jay.lansiquot@pm.me>
2022-05-01 12:22:09 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,8 @@ cd $HOME
 
 # Install Brew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> $HOME/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Development
 brew install git
